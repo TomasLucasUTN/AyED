834f7ebc81ff0ba915997d309b9431534f8b3ef2
Tomas Lucas UTN <tlucas@frba.utn.edu.ar>
2023-05-20 19:48:57 -0300

    Add files via upload

---
+++ 01-CalcularPi/main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+double pi=1;
+double n=1;
+int ciclos=0;
+
+double calcularPi(double pi){
+//  3,141592/4 = 0.7853980
+    while(pi<=(0.7853980) || pi>=(0.7853982)){
+        if (n>0){
+            n+=2;
+            n*=-1;
+        } else{
+            n*=-1;
+            n+=2;    
+        }
+        pi+=(1/n);
+        ciclos ++;
+    }
+        pi*=4;
+
+    return pi;
+}
+
+
+int main(){
+
+    cout<<setprecision(7)<<calcularPi(pi);
+    cout<<"\nEn "<<ciclos<<" ciclos";
+
+    return 0;
+}
